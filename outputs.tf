3e064fc2be63d9fb479c2c90f59a54a4cf464445
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "spring_cloud_builders" {
-  description = "All spring_cloud_builder resources"
-  value       = azurerm_spring_cloud_builder.spring_cloud_builders
-}
 output "spring_cloud_builders_build_pack_group" {
-  description = "List of build_pack_group values across all spring_cloud_builders"
-  value       = [for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : v.build_pack_group]
+  description = "Map of build_pack_group values across all spring_cloud_builders, keyed the same as var.spring_cloud_builders"
+  value       = { for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : k => v.build_pack_group }
 }
 output "spring_cloud_builders_name" {
-  description = "List of name values across all spring_cloud_builders"
-  value       = [for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : v.name]
+  description = "Map of name values across all spring_cloud_builders, keyed the same as var.spring_cloud_builders"
+  value       = { for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : k => v.name }
 }
 output "spring_cloud_builders_spring_cloud_service_id" {
-  description = "List of spring_cloud_service_id values across all spring_cloud_builders"
-  value       = [for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : v.spring_cloud_service_id]
+  description = "Map of spring_cloud_service_id values across all spring_cloud_builders, keyed the same as var.spring_cloud_builders"
+  value       = { for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : k => v.spring_cloud_service_id }
 }
 output "spring_cloud_builders_stack" {
-  description = "List of stack values across all spring_cloud_builders"
-  value       = [for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : v.stack]
+  description = "Map of stack values across all spring_cloud_builders, keyed the same as var.spring_cloud_builders"
+  value       = { for k, v in azurerm_spring_cloud_builder.spring_cloud_builders : k => v.stack }
 }
 
